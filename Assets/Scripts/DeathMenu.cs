0aada13751fd1a49ad92fe55f96d430ab2f01c79
MironovVl <vmir-2001@yandex.ru>
2021-04-02 22:18:58 +0300

    with death

---
+++ Assets/Scripts/DeathMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathMenu : MonoBehaviour
+{
+    public GameObject deathObject;
+    public PlayerControll player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        deathObject.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.HP <= 0)
+        {
+            deathObject.gameObject.SetActive(true);
+        }
+
+
+    }
+    public void ReloadLeavel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
